c8c4a105f97ce6e2c71aad02242149e0dbe408b1
Nikai Steur <nikai.steur@iconum.com>
2019-05-06 16:39:22 +0200

    blog

---
+++ website/blog/2019-05-06-new-app-version.md
@@ -0,0 +1,8 @@
+---
+title: New App Version
+author: Nikai Steur
+authorURL: https://www.linkedin.com/in/nikaisteur/
+authorFBID: 10154307453656869
+---
+
+After succesfully testing our app prototype in production, we are now refactoring and rebuilding the app in Ionic 4.0
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,15 +23,15 @@ const users = [
 const siteConfig = {
   title: 'OrderField Documentation', // Title for your website.
   tagline: 'Testing ',
-  url: 'https://N1k41.github.io', // Your website URL
-  baseUrl: '/orderfield-documentation/', // Base URL for your project */
+  url: 'https://n1k41.github.io', // Your website URL
+  baseUrl: '/orderfield-docs/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'orderfield-documentation',
-  organizationName: 'N1k41',
+  projectName: 'orderfield-docs',
+  organizationName: 'n1k41',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
